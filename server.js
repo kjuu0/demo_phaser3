338e9c3f3b29d9806dcaf7c5a4b2c6c2269bfe4a
Brian Qiu <brianlqiu5@gmail.com>
2020-04-16 12:06:41 -0700

    deploying on heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 3000;
 const app = require('express')(); //requires express module and creates instance of express
 const server = require('http').Server(app); //allows server to handle http
 const io = require('socket.io')(server);
@@ -6,8 +7,6 @@ const next = require('next');
 const dev = process.env.NODE_ENV != 'production';
 const nextApp = next({dev});
 const nextHandler = nextApp.getRequestHandler();
-
-let port = 3000;
 
 let players = {}; //stores all players in an object
 
